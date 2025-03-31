e14b66dd735f935b0e6744782f4707cc4e3793dd
WarGhost132 <wawghostick@gmail.com>
2025-03-31 14:37:32 +0300

    style page

--- vue/src/shared/ui/Page/Page.vue
+++ vue/src/shared/ui/Page/Page.vue
@@ -1,26 +1,11 @@
 <script lang="ts" setup>
-
+import styles from './styles.module.scss'
 </script>
 
 <template>
-  <div class="container">
-    <div class="content">
+  <div :class="styles['container']">
+    <div :class="styles['content']">
       <slot />
     </div>
   </div>
 </template>
-
-<style scoped>
-.container {
-  min-height: 100svh;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.content {
-  width: 100%;
-  max-width: 700px;
-}
-</style>
